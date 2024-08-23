85af161338ae77dd17a333d934caefe44ecc307b
Estebana Altamiranda <jaltamiranda187@gmail.com>
2024-08-23 00:42:02 +0000

    creacion de spring boot

---
+++ colombina-app/src/main/java/com/colombina/colombina_app/ColombinaAppApplication.java
@@ -0,0 +1,13 @@
+package com.colombina.colombina_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColombinaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColombinaAppApplication.class, args);
+	}
+
+}
---
+++ colombina-app/src/test/java/com/colombina/colombina_app/ColombinaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colombina.colombina_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColombinaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
