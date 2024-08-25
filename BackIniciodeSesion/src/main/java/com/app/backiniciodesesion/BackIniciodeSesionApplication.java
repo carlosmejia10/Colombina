120d5e33f60e38f107d0ec29bd81c64e549a0ead
Pablo Gonzalez <140916704+PabloGonzalez278@users.noreply.github.com>
2024-08-24 20:37:04 -0500

    Backend Inicio de sesion

---
+++ BackIniciodeSesion/src/main/java/com/app/backiniciodesesion/BackIniciodeSesionApplication.java
@@ -0,0 +1,13 @@
+package com.app.backiniciodesesion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackIniciodeSesionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackIniciodeSesionApplication.class, args);
+    }
+
+}
---
+++ BackIniciodeSesion/src/main/java/com/app/backiniciodesesion/services/UserDetailsServiceImpl.java
@@ -0,0 +1,34 @@
+package com.app.backiniciodesesion.services;
+import com.app.backiniciodesesion.models.Usuario;
+import com.app.backiniciodesesion.repositories.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UserDetailsServiceImpl(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Usuario> user = usuarioRepository.findByUsername(username);
+        if (user.isPresent()) {
+            return org.springframework.security.core.userdetails.User.builder()
+                    .username(user.get().getUsername())
+                    .password(user.get().getPassword())
+                    .roles(user.get().getRole())
+                    .build();
+        } else {
+            throw new UsernameNotFoundException("Usuario no encontrado");
+        }
+    }
+}
+
+
