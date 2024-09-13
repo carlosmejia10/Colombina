f13ad6ae3773487a72cb5258332664068cb428e9
Estebana Altamiranda <jaltamiranda187@gmail.com>
2024-09-13 00:28:08 +0000

    Creacion Del Controlador Ususario

--- colombina/src/main/java/com/example/colombina/entidad/DatabaseInit.java
+++ colombina/src/main/java/com/example/colombina/entidad/DatabaseInit.java
@@ -1,15 +1,17 @@
 package com.example.colombina.entidad;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Controller;
+
+import com.example.colombina.repositorio.UsuarioRepository;
 
 import jakarta.transaction.Transactional;
 
 //Implementar clases de repositorio para inicializar la base de datos
 
-//@Autowired
-//UsuarioRepository usuarioRepository;
+
 
 //...
 
@@ -17,9 +19,16 @@ import jakarta.transaction.Transactional;
 @Transactional
 public class DatabaseInit implements ApplicationRunner {
 
+        @Autowired
+        UsuarioRepository usuarioRepository;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         //Implementacion del repositorio
+
+        //Agrgar usuarios en base de datos
+        
+
     }
     
 }
