b74c79704b6f436b644e90e3af57393e593f347d
Estebana Altamiranda <jaltamiranda187@gmail.com>
2024-09-29 19:27:43 +0000

    cambios para que pueda compilar

--- colombina/src/main/java/com/example/colombina/repositorio/TramiteRepository.java
+++ colombina/src/main/java/com/example/colombina/repositorio/TramiteRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.colombina.entidad.Tramite;
 import com.example.colombina.entidad.Usuario;
 public interface TramiteRepository extends JpaRepository <Tramite, Long>{
-    Optional<Tramite> findById(String cedula);
     List<Tramite> findByTipo(String tipo);
     List<Tramite> findByUsuario(Usuario usuario);
     List<Tramite> findByTitulo(String titulo);
