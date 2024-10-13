7af9e546e38d8fc71dfc76bf00d11d626775b7a1
alejandrocanadas <alejandrocanadas11@gmail.com>
2024-10-13 18:56:48 -0500

    Add files via upload
    Pruebas inisio de sesión pantalla 1

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,24 @@
+describe('template spec', () => {
+  it('Inisio de Sesion', () => {
+    cy.visit('http://localhost:4200/')
+    cy.get('#username').type("JuanPablo")
+    cy.get('#password').type("123456")
+    cy.get('.fl').click()
+    cy.url().should('include', 'http://localhost:4200/tramites')
+  })
+
+  it('recordar usuario', () => {
+    cy.visit('http://localhost:4200/')
+    cy.get('#username').type("JuanPablo")
+    cy.get('#password').type("123456")
+    cy.get(':nth-child(1) > .activar > label').click().check()
+  })
+
+  /*it('Campos vacios', () => {
+    cy.visit('http://localhost:4200/')
+    cy.get('.fl').click()
+    cy.get('.alert-danger').should('be.visible')
+    cy.url().should('include', 'http://localhost:4200/tramites')
+  })
+})
+
