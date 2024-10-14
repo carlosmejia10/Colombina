0b952db00233dab5af5aa58a77373da082a22392
eugeniadayoub <eugeniav.dayoub@javeriana.edu.co>
2024-10-13 21:21:43 -0500

    Update visualizacion_tramites.cy.js

--- cypress/e2e/visualizacion_tramites.cy.js
+++ cypress/e2e/visualizacion_tramites.cy.js
@@ -1,5 +1,5 @@
 describe('template spec', () => {
-  it('Campos Vacios', () => {
+  it('Barra de progreso', () => {
     cy.visit('http://localhost:4200/')
     cy.get('#username').type("JuanPablo")
     cy.get('#password').type("123456")
@@ -9,7 +9,7 @@ describe('template spec', () => {
   })
 
   describe('template spec', () => {
-    it('Campos Vacios', () => {
+    it('Ver documentos', () => {
       cy.visit('http://localhost:4200/tramites')
       cy.get('.registro-info > :nth-child(10)').click()
       cy.url().should('include', 'http://localhost:4200/tramites/verdocumentos') //Debería mandar a ver los documentos del trámite
@@ -17,7 +17,7 @@ describe('template spec', () => {
   })
 
   describe('template spec', () => {
-    it('Campos Vacios', () => {
+    it('Tramites pendientes', () => {
       cy.visit('http://localhost:4200/tramites')
       cy.get('.registro-info > :nth-child(8)').click()
       cy.url().should('include', 'http://localhost:4200/tramites/pendientes') //Debería mandar a ver los trámites pendientes
